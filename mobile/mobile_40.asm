0e0f920b75a035a2e8840337ca71aaa3cf0537a1
gb-mobile <110418063+gb-mobile@users.noreply.github.com>
2024-04-22 21:56:12 -0400

    Trade text co ord fixes

--- mobile/mobile_40.asm
+++ mobile/mobile_40.asm
@@ -5990,14 +5990,14 @@ Jumptable_1029cb:
 
 Function1029cf:
 	call LoadStandardMenuHeader
-	hlcoord 10, 7
+	hlcoord 7, 7
 	ld b, 3
-	ld c, 7
+	ld c, 10
 	ld d, h
 	ld e, l
 	farcall _LinkTextbox
 	ld de, String_102a26
-	hlcoord 12, 8
+	hlcoord 9, 8
 	call PlaceString
 	ld hl, wcd4b
 	set 1, [hl]
